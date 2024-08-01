40a9af05297aa2aa342c25332132391d19ce54b3
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-08-01 13:02:55 +0200

    final

---
+++ src/modals/contacts.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+const contactSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: Number,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: false,
+    },
+    isFavourite: {
+      type: Boolean,
+      default: false,
+    },
+    contactType: {
+      type: String,
+      enum: ['work', 'home', 'personal'],
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+const Contact = mongoose.model('Contact', contactSchema);
+
+export { Contact };
