be9eae3680b97b410c2188dd5e0aa3229326759f
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-09-08 13:51:36 +0200

    1

--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -1,17 +1,34 @@
 import createHttpError from 'http-errors';
 
-export function validateBody(schema) {
-  return (req, res, next) => {
-    const result = schema.validate(req.body, { abortEarly: false });
+// export function validateBody(schema) {
+//   return async (req, res, next) => {
+//     const result = await schema.validate(req.body, { abortEarly: false });
 
-    if (typeof result.error !== 'undefined') {
-      return next(
+//     if (typeof result.error !== 'undefined') {
+//       return next(
+//         createHttpError(
+//           400,
+//           result.error.details.map((err) => err.message).join(', '),
+//         ),
+//       );
+//     }
+//     next();
+//   };
+// }
+
+export function validateBody(schema) {
+  return async (req, res, next) => {
+    try {
+      await schema.validateAsync(req.body, { abortEarly: false });
+
+      next();
+    } catch (err) {
+      next(
         createHttpError(
           400,
-          result.error.details.map((err) => err.message).join(', '),
+          err.details.map((detail) => detail.message).join(','),
         ),
       );
     }
-    next();
   };
 }
